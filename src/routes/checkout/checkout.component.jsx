65acd0a830e8ea5f4c275d286ce83d24b352a84a
Vigneshwar <35205545+imvicky710@users.noreply.github.com>
2022-10-20 21:10:37 +0530

    Stripe

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -2,6 +2,8 @@ import { CheckoutContainer, CheckoutHeader, HeaderBlock, Total } from './checkou
 import { useSelector } from 'react-redux';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 import { selectCartItems, selectCartTotal } from '../../store/cart/cart.selector';
+import PaymentForm from '../../components/payment-form/payment-form.component';
+
 const Checkout = () => {
     const cartItems = useSelector(selectCartItems);
     const cartTotal = useSelector(selectCartTotal);
@@ -26,6 +28,7 @@ const Checkout = () => {
             </CheckoutHeader>
             {cartItems.map((cartItem) => <CheckoutItem key={cartItem.id} cartItem={cartItem} />)}
             <Total>Total: ${cartTotal}</Total>
+            <PaymentForm />
         </CheckoutContainer>
     )
 }
